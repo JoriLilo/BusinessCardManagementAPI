27e877335ccc2dd3c7627ff1dc5da2dffb765c16
= <joridalilollari27@gamil.com>
2025-12-14 13:40:09 +0100

    adding the controllers

--- src/main/java/com/example/BusinessCardManagement/controller/CardController.java
+++ src/main/java/com/example/BusinessCardManagement/controller/CardController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/Card")
+@RequestMapping("/Card")  // the path
 public class CardController {
 
     public final CardService cardService;
